ad2ce0feb9b8cb32bd547988fdb8ba0e00cc45b8
I M Gangu <migangu66@gmail.com>
2024-02-02 12:06:34 +0530

    Connected to DB

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connectedInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}`
+    );
+    console.log(`\n MongoDB connected!! DB HOST: ${connectedInstance.connection.host}`);
+  } catch (error) {
+    console.log("MongoDB connection error: " + error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello world!");
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const port = process.env.PORT || 8080;
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running on port : ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Connection failed", err);
+  });
